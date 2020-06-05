4b3b9af2f2803040b1ba9bc341f418687e620889
Trang Q. Ngo <tqn1@williams.edu>
2020-06-05 17:05:27 +0700

    Updates

---
+++ precache-manifest.3b8915788cdd075cba7a9c79340aa0a6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c32d07ae35088b6d4cf0332d544db2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "838e7ecd7f0173713b34",
+    "url": "/static/css/main.bd8f0025.chunk.css"
+  },
+  {
+    "revision": "a7cd45adf8d467300be8",
+    "url": "/static/js/2.aec85aa2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.aec85aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "838e7ecd7f0173713b34",
+    "url": "/static/js/main.a61b074a.chunk.js"
+  },
+  {
+    "revision": "9d7ec453320509f6d66c",
+    "url": "/static/js/runtime-main.42cfb24a.js"
+  },
+  {
+    "revision": "274e78c66f04bb49dabf4b2bcb171971",
+    "url": "/static/media/place-holder.274e78c6.jpg"
+  },
+  {
+    "revision": "e80560bfeedafe8af201504f7eaae7ec",
+    "url": "/static/media/q-camp.e80560bf.PNG"
+  },
+  {
+    "revision": "e3587920d29a4181c7580b86746982c5",
+    "url": "/static/media/seulgi-psycho.e3587920.PNG"
+  },
+  {
+    "revision": "c1d29e3f6e8a3f1a0cdc651cecaba0ad",
+    "url": "/static/media/stars.c1d29e3f.PNG"
+  },
+  {
+    "revision": "3d6de6455a5f4e2663001020e408f129",
+    "url": "/static/media/wifi-cats.3d6de645.PNG"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a90264f063c20c7daef88e85a1c22ddc.js"
+  "/precache-manifest.3b8915788cdd075cba7a9c79340aa0a6.js"
 );
 
 self.addEventListener('message', (event) => {
